b752f1a248c43f103123dbc2961ff8ba96fef847
LazarevicV <vukadinlazarevic@gmail.com>
2023-11-26 23:36:13 +0100

    user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -30,5 +30,50 @@ class UserSeeder extends Seeder
         $user->created_at = date('Y-m-d H:i:s');
         $user->updated_at = date('Y-m-d H:i:s');
         $user->save();
+
+        $user = new User();
+        $user->ime_prezime = 'Milica Milic';
+        $user->email = 'milicamilic@gmail.com';
+        $user->access_level = 'user';
+        $user->password = bcrypt('milicamilica');
+        $user->created_at = date('Y-m-d H:i:s');
+        $user->updated_at = date('Y-m-d H:i:s');
+        $user->save();
+
+        $user = new User();
+        $user->ime_prezime = 'Nikola Nikolic';
+        $user->email = 'nikolanikolic@gmail.com';
+        $user->access_level = 'user';
+        $user->password = bcrypt('nikolanikola');
+        $user->created_at = date('Y-m-d H:i:s');
+        $user->updated_at = date('Y-m-d H:i:s');
+        $user->save();
+
+        $user = new User();
+        $user->ime_prezime = 'Zika Zikic';
+        $user->email = 'zikazikic@gmail.com';
+        $user->access_level = 'user';
+        $user->password = bcrypt('zikazika');
+        $user->created_at = date('Y-m-d H:i:s');
+        $user->updated_at = date('Y-m-d H:i:s');
+        $user->save();
+
+        $user = new User();
+        $user->ime_prezime = 'Prle Prlic';
+        $user->email = 'prleprlic@gmail.com';
+        $user->access_level = 'user';
+        $user->password = bcrypt('prleprle');
+        $user->created_at = date('Y-m-d H:i:s');
+        $user->updated_at = date('Y-m-d H:i:s');
+        $user->save();
+
+        $user = new User();
+        $user->ime_prezime = 'Krle Krlic';
+        $user->email = 'krlekrlic@gmail.com';
+        $user->access_level = 'user';
+        $user->password = bcrypt('krlekrle');
+        $user->created_at = date('Y-m-d H:i:s');
+        $user->updated_at = date('Y-m-d H:i:s');
+        $user->save();
     }
 }
